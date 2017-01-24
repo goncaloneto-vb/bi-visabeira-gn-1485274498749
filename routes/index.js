6b2f1185233c2943bc413c9e0b12213ea24190b9
Gonçalo Neto <goncaloneto@grupovisabeira.com>
2017-01-24 11:17:56 -0600

    novas alteracoes

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,3 @@
 exports.index = function(req, res){
   res.render('index.html', { title: 'Cloudant Boiler Plate' });
 };
-exports.home = function(req, res){
-  res.render('home.html', { title: 'home page' });
-};
